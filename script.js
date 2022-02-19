0fee3101c7aee2a15b6dcaf24a748c51a7759160
Rafael <rafadoria1@gmail.com>
2022-02-19 14:22:52 +0900

    Added basic display functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let displayValue = 0
+
+const buttons = document.querySelectorAll('.btn')
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const display = document.getElementById('displayMain')
+
+display.textContent = displayValue
+
+
+
 function add(a, b) {
     return a + b
 }
